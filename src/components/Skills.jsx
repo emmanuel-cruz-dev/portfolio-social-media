1e19f4d502aafba27bced07d20d2ead894cfaccd
Emmanuel Cruz <EmmaLCruz@users.noreply.github.com>
2024-12-25 17:44:14 -0300

    orden skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -42,7 +42,7 @@ const Skills = () => {
             <h3 className="text-lg">{t("sections.skills.description")}</h3>
           </div>
         </AnimatedComponent>
-        <article className="flex flex-col gap-8">
+        <article className="flex justify-center items-center flex-wrap gap-8">
           {/* Desarrollo Web */}
           <article
             className={`${
@@ -79,140 +79,138 @@ const Skills = () => {
             </article>
           </article>
 
-          <div className="flex flex-wrap gap-8 justify-center">
-            {/* Frameworks y Bibliotecas */}
-            <article
-              className={`${
-                theme === "dark" ? "shadow__dark" : "shadow__light"
-              } w-full md:w-fit flex flex-col gap-8 items-center text-center bg-sky-300/20 py-6 px-10 border-2 border-sky-300/20 hover:bg-transparent transition-colors duration-300`}
-            >
-              <h4 className="font-semibold text-xl">
-                {t("sections.skills.subtitle2")}
-              </h4>
+          {/* Herramientas de Desarrollo */}
+          <article
+            className={` ${
+              theme === "dark" ? "shadow__dark" : "shadow__light"
+            } w-full md:w-fit flex flex-col gap-8 items-center text-center bg-sky-300/20 py-6 px-10 border-2 border-sky-300/20 hover:bg-transparent transition-colors duration-300`}
+          >
+            <h4 className="font-semibold text-xl">
+              {t("sections.skills.subtitle3")}
+            </h4>
+            <article className="flex flex-wrap gap-6 lg:gap-14 justify-around items-center">
+              <SkillsCard
+                title={t("sections.skills.vscode")}
+                src={VsCodeLogo}
+                alt="VS Code"
+              />
 
-              <article className="flex flex-wrap gap-6 lg:gap-14 justify-around items-center">
-                <SkillsCard
-                  title={t("sections.skills.react")}
-                  src={ReactLogo}
-                  alt="React"
-                />
+              <SkillsCard
+                title={t("sections.skills.git")}
+                src={GitLogo}
+                alt="Git"
+              />
 
-                <SkillsCard
-                  title={t("sections.skills.sass")}
-                  src={SassLogo}
-                  alt="Sass"
-                />
-
-                <SkillsCard
-                  title={t("sections.skills.tailwind")}
-                  src={TailwindLogo}
-                  alt="Tailwind CSS"
-                />
-
-                <SkillsCard
-                  title={t("sections.skills.bootstrap")}
-                  src={BootstrapLogo}
-                  alt="Bootstrap"
-                />
-
-                <SkillsCard
-                  title={t("sections.skills.nodejs")}
-                  src={NodeLogo}
-                  alt="Node.js"
-                />
-              </article>
+              <SkillsCard
+                title={t("sections.skills.github")}
+                src={GithubLogo}
+                alt="GitHub"
+              />
             </article>
+          </article>
 
-            {/* Herramientas de Desarrollo */}
-            <article
-              className={` ${
-                theme === "dark" ? "shadow__dark" : "shadow__light"
-              } w-full md:w-fit flex flex-col gap-8 items-center text-center bg-sky-300/20 py-6 px-10 border-2 border-sky-300/20 hover:bg-transparent transition-colors duration-300`}
-            >
-              <h4 className="font-semibold text-xl">
-                {t("sections.skills.subtitle3")}
-              </h4>
-              <article className="flex flex-wrap gap-6 lg:gap-14 justify-around items-center">
-                <SkillsCard
-                  title={t("sections.skills.vscode")}
-                  src={VsCodeLogo}
-                  alt="VS Code"
-                />
+          {/* Frameworks y Bibliotecas */}
+          <article
+            className={`${
+              theme === "dark" ? "shadow__dark" : "shadow__light"
+            } w-full md:w-fit flex flex-col gap-8 items-center text-center bg-sky-300/20 py-6 px-10 border-2 border-sky-300/20 hover:bg-transparent transition-colors duration-300`}
+          >
+            <h4 className="font-semibold text-xl">
+              {t("sections.skills.subtitle2")}
+            </h4>
 
-                <SkillsCard
-                  title={t("sections.skills.git")}
-                  src={GitLogo}
-                  alt="Git"
-                />
+            <article className="flex flex-wrap gap-6 lg:gap-14 justify-around items-center">
+              <SkillsCard
+                title={t("sections.skills.react")}
+                src={ReactLogo}
+                alt="React"
+              />
 
-                <SkillsCard
-                  title={t("sections.skills.github")}
-                  src={GithubLogo}
-                  alt="GitHub"
-                />
-              </article>
+              <SkillsCard
+                title={t("sections.skills.sass")}
+                src={SassLogo}
+                alt="Sass"
+              />
+
+              <SkillsCard
+                title={t("sections.skills.tailwind")}
+                src={TailwindLogo}
+                alt="Tailwind CSS"
+              />
+
+              <SkillsCard
+                title={t("sections.skills.bootstrap")}
+                src={BootstrapLogo}
+                alt="Bootstrap"
+              />
+
+              <SkillsCard
+                title={t("sections.skills.nodejs")}
+                src={NodeLogo}
+                alt="Node.js"
+              />
             </article>
+          </article>
 
-            {/* Diseño y Prototipado */}
-            <article
-              className={`${
-                theme === "dark" ? "shadow__dark" : "shadow__light"
-              } w-full md:w-fit flex flex-col gap-8 items-center text-center bg-sky-300/20 py-6 px-10 border-2 border-sky-300/20 hover:bg-transparent transition-colors duration-300`}
-            >
-              <h4 className="font-semibold text-xl">
-                {t("sections.skills.subtitle4")}
-              </h4>
-              <article className="flex flex-wrap gap-6 lg:gap-14 justify-around items-center">
-                <SkillsCard
-                  title={t("sections.skills.figma")}
-                  src={FigmaLogo}
-                  alt="Figma"
-                />
+          {/* Diseño y Prototipado */}
+          <article
+            className={`${
+              theme === "dark" ? "shadow__dark" : "shadow__light"
+            } w-full md:w-fit flex flex-col gap-8 items-center text-center bg-sky-300/20 py-6 px-10 border-2 border-sky-300/20 hover:bg-transparent transition-colors duration-300`}
+          >
+            <h4 className="font-semibold text-xl">
+              {t("sections.skills.subtitle4")}
+            </h4>
+            <article className="flex flex-wrap gap-6 lg:gap-14 justify-around items-center">
+              <SkillsCard
+                title={t("sections.skills.figma")}
+                src={FigmaLogo}
+                alt="Figma"
+              />
 
-                <SkillsCard
-                  title={t("sections.skills.photoshop")}
-                  src={PhotoshopLogo}
-                  alt="Photoshop"
-                />
+              <SkillsCard
+                title={t("sections.skills.photoshop")}
+                src={PhotoshopLogo}
+                alt="Photoshop"
+              />
 
-                <SkillsCard
-                  title={t("sections.skills.webflow")}
-                  src={WebflowLogo}
-                  alt="Webflow"
-                />
-              </article>
+              <SkillsCard
+                title={t("sections.skills.webflow")}
+                src={WebflowLogo}
+                alt="Webflow"
+              />
             </article>
+          </article>
 
-            {/* Metodologías */}
-            <article
-              className={`${
-                theme === "dark" ? "shadow__dark" : "shadow__light"
-              } w-full md:w-fit flex flex-col gap-8 items-center text-center bg-sky-300/20 py-6 px-10 border-2 border-sky-300/20 hover:bg-transparent transition-colors duration-300`}
-            >
-              <h4 className="font-semibold text-xl">
-                {t("sections.skills.subtitle5")}
-              </h4>
-              <article className="flex flex-wrap gap-6 lg:gap-14 justify-around items-center">
-                <SkillsCard
-                  title={t("sections.skills.mobileFirst")}
-                  src={MobileFirstLogo}
-                  alt="Mobile-first"
-                />
+          {/* Metodologías */}
+          <article
+            className={`${
+              theme === "dark" ? "shadow__dark" : "shadow__light"
+            } w-full md:w-fit flex flex-col gap-8 items-center text-center bg-sky-300/20 py-6 px-10 border-2 border-sky-300/20 hover:bg-transparent transition-colors duration-300`}
+          >
+            <h4 className="font-semibold text-xl">
+              {t("sections.skills.subtitle5")}
+            </h4>
+            <article className="flex flex-wrap gap-6 lg:gap-14 justify-around items-center">
+              <SkillsCard
+                title={t("sections.skills.mobileFirst")}
+                src={MobileFirstLogo}
+                alt="Mobile-first"
+              />
 
-                <SkillsCard
-                  title={t("sections.skills.responsive")}
-                  src={ResponsiveLogo}
-                  alt="Responsive Design"
-                />
+              <SkillsCard
+                title={t("sections.skills.responsive")}
+                src={ResponsiveLogo}
+                alt="Responsive Design"
+              />
 
-                <SkillsCard
-                  title={t("sections.skills.bem")}
-                  src={BemLogo}
-                  alt="BEM"
-                />
-              </article>
+              <SkillsCard
+                title={t("sections.skills.bem")}
+                src={BemLogo}
+                alt="BEM"
+              />
             </article>
-          </div>
+          </article>
         </article>
       </section>
     </main>
