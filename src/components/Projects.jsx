2b2253d1632d715ec2cd87151e8b35b4ac63afef
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-01-19 15:22:31 -0300

    corrigiendo enlaces de proyectos en Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -41,8 +41,8 @@ const Projects = () => {
         { name: "Javascript", img: JavascriptLogo },
         { name: "Jquery", img: JqueryLogo },
       ],
-      link: "https://emmalcruz.github.io/los-pollos-hermanos-website/",
-      github: "https://github.com/EmmaLCruz/los-pollos-hermanos-website",
+      link: "https://emmanuel-cruz-dev.github.io/los-pollos-hermanos-website/",
+      github: "https://github.com/emmanuel-cruz-dev/los-pollos-hermanos-website",
     },
     {
       id: 2,
