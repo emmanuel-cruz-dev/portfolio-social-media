c7502e8fd73505752bf52115758a8f58eda8a6bc
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-03-29 13:46:05 -0300

    refactor: utils creado, scrollToTop function creada

--- src/components/ui/ScrollToTopButton.jsx
+++ src/components/ui/ScrollToTopButton.jsx
@@ -1,20 +1,17 @@
 import useVisibility from "../../hook/useVisibility";
+import { scrollToTop } from "../../utils/utils";
 import { useTheme } from "../ThemeContext";
 
 function ScrollToTopButton() {
   const { isVisible, progress } = useVisibility();
   const { theme } = useTheme();
 
-  const handleClick = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
-
   return (
     <button
       className={`up-btn fixed bottom-6 right-6 z-50 bg-colorTertiary hover:bg-black text-white rounded-full cursor-pointer transition-all duration-300 w-14 h-14 flex justify-center items-center ${
         isVisible ? "opacity-100" : "opacity-0"
       }`}
-      onClick={handleClick}
+      onClick={scrollToTop}
     >
       <div
         className="absolute top-0 left-0 w-full h-full rounded-full"
