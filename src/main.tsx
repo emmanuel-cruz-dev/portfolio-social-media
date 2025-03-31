8eb14b5968da33857c6bf252c597079e8345cac7
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-03-31 15:57:31 -0300

    refactor: cambiando extensiones en main, App y ThemeContext

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import "./styles/main.scss";
 import "./i18n/languages.js";
-import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { ThemeProvider } from "./context/ThemeContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
