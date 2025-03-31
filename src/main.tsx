0c6a166e5d433ef420a165ff822edbf85e4268ae
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-03-31 16:29:45 -0300

    refactor: index html src de main actualizado

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/main.scss";
-import "./i18n/languages.js";
+import "./i18n/languages.ts";
 import { ThemeProvider } from "./context/ThemeContext";
 
 const rootElement = document.getElementById("root");
