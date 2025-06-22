6a04520674dcd5f8bca612566051b73d1ab11068
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-06-22 15:22:54 -0300

    feat: updates structure and style hover effects in SkillsCard component

--- src/sections/Skills/SkillsCard.tsx
+++ src/sections/Skills/SkillsCard.tsx
@@ -3,15 +3,14 @@ import { SkillsCardProps } from "src/types/types";
 
 const SkillsCard: FC<SkillsCardProps> = ({ src, alt, title }) => {
   return (
-    <div className="card">
+    <div className="card group" title={title} aria-label={alt}>
       <img
         src={src}
         alt={`${alt} logo`}
-        title={title}
         loading="lazy"
         width={900}
         height={900}
-        className="w-16 hover:scale-110 transition-transform duration-300"
+        className="w-16 group-hover:scale-110 transition-transform duration-300"
       />
       <p className="font-semibold">{alt}</p>
     </div>
